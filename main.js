4483386cbc1305e6b294614fa12f3a89e0fb5e15
Stephen Fox <sfoxorama@gmail.com>
2017-09-15 19:49:41 -0400

    Refactor main.js to clean up code and use ES6 features. Add eslint config. SCSS style

--- main.js
+++ main.js
@@ -1,32 +1,25 @@
-(function() {
-  function setIframeSource (url) {
-    let youtubeId = parseYoutubeId(url);
-    document.getElementById('videoFrame').src = `https://www.youtube.com/embed/${youtubeId}`;
-  }
+(function () {
+    const setIframeSource = (url) => document.getElementById('videoFrame').src = `https://www.youtube.com/embed/${parseYoutubeId(url)}`
 
-  function parseYoutubeId(url) {
-    return url.match(/(^|=|\/)([0-9A-Za-z_-]{11})(\/|&|$|\?|#)/)[2];
-  }
+    const parseYoutubeId = (url) => url.match(/(^|=|\/)([0-9A-Za-z_-]{11})(\/|&|$|\?|#)/)[2]
 
-  function setByButton () {
-    let url = document.getElementById('videoUrl').value;
-    return url;
-  }
+    const getUrlInput = () => document.getElementById('videoUrl').value
 
-  function setByParam () {
-    let params = new URLSearchParams(document.location.search.substring(1));
-    let url = params.get("url");
-    if (url) {
-      setIframeSource(url);
-      document.getElementById('videoUrl').value = url;
+    const getUrlParam = () => {
+        const params = new URLSearchParams(document.location.search.substring(1))
+        const url = params.get('url')
+        document.getElementById('videoUrl').value = url
+        return url
     }
-  }
 
-  document.getElementById('goButton').addEventListener('click', (event) => {
-    event.preventDefault();
-    setIframeSource(setByButton());
-  });
+    document.getElementById('goButton').addEventListener('click', (event) => {
+        event.preventDefault()
+        setIframeSource(getUrlInput())
+    })
 
-  setByParam();
+    const urlParam = getUrlParam()
+    if (urlParam) {
+        setIframeSource(urlParam)
+    }
 
-})();
+})()
