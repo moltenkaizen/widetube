6f42a096c81451e69537d549c3e1f5c98cd023d5
Stephen Fox <sfoxorama@gmail.com>
2017-09-15 19:51:15 -0400

    Move from 4 to 2 space indents

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 (function () {
-    const setIframeSource = (url) => document.getElementById('videoFrame').src = `https://www.youtube.com/embed/${parseYoutubeId(url)}`
+  const setIframeSource = (url) => document.getElementById('videoFrame').src = `https://www.youtube.com/embed/${parseYoutubeId(url)}`
 
-    const parseYoutubeId = (url) => url.match(/(^|=|\/)([0-9A-Za-z_-]{11})(\/|&|$|\?|#)/)[2]
+  const parseYoutubeId = (url) => url.match(/(^|=|\/)([0-9A-Za-z_-]{11})(\/|&|$|\?|#)/)[2]
 
-    const getUrlInput = () => document.getElementById('videoUrl').value
+  const getUrlInput = () => document.getElementById('videoUrl').value
 
-    const getUrlParam = () => {
-        const params = new URLSearchParams(document.location.search.substring(1))
-        const url = params.get('url')
-        document.getElementById('videoUrl').value = url
-        return url
-    }
+  const getUrlParam = () => {
+    const params = new URLSearchParams(document.location.search.substring(1))
+    const url = params.get('url')
+    document.getElementById('videoUrl').value = url
+    return url
+  }
 
-    document.getElementById('goButton').addEventListener('click', (event) => {
-        event.preventDefault()
-        setIframeSource(getUrlInput())
-    })
+  document.getElementById('goButton').addEventListener('click', (event) => {
+    event.preventDefault()
+    setIframeSource(getUrlInput())
+  })
 
-    const urlParam = getUrlParam()
-    if (urlParam) {
-        setIframeSource(urlParam)
-    }
+  const urlParam = getUrlParam()
+  if (urlParam) {
+    setIframeSource(urlParam)
+  }
 
 })()
